9e1e27cae21a3834f935b7817037e4ce9dc30cb5
James Arthur <jamesarthur7@icloud.com>
2019-12-30 20:23:01 +0000

    Updated date tp later in Jan?!
    * James still has no idea when KOPPA2 will happen XD

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 10, 2019 17:00:00").getTime();
+var countDownDate = new Date("Jan 20, 2019 17:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
