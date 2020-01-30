f114899a0dd0c53c270a3eab59f55920936b43ff
malawrence1020 <57117279+malawrence1020@users.noreply.github.com>
2020-01-30 19:39:39 +0000

    REVERRRRRRRRRRT

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -13,43 +13,9 @@ var x = setInterval(function() {
         document.getElementById("countdown").innerHTML = "Welcome to NT";
   }
   
-  else if (distance > 8*3600) {
+  else if (distance > 0) {
         document.getElementById("countdown").innerHTML ="Welcome to NT! ϙ2 begins in: " + hours + "h "
         + minutes + "m " + seconds + "s ";
-  }
-  
-  else if (distance > 4*3600) {
-        document.getElementById("countdown").innerHTML ="Welcome to NT! James is on his way! Matthew is in college. ϙ2 begins in: 
-        " + hours + "h " + minutes + "m " + seconds + "s ";
-  }
-  
-  else if (distance > 3*3600) {
-        document.getElementById("countdown").innerHTML ="Welcome to NT! James is in devil country! Matthew is still in college. ϙ2 begins in: 
-        " + hours + "h " + minutes + "m " + seconds + "s ";
-  }
-  
-  else if (distance > 2*3600) {
-        document.getElementById("countdown").innerHTML ="Welcome to NT! James is is Axminster. I don't know where Matthew is. ϙ2 begins in: 
-        " + hours + "h " + minutes + "m " + seconds + "s ";
-  }
-  
-  else if (distance > 3600) {
-        document.getElementById("countdown").innerHTML ="Welcome to NT! James is in Chocky Town! Matthew is probably at home. ϙ2 begins in: 
-        " + hours + "h " + minutes + "m " + seconds + "s ";
-  }
-  
-  else if (distance > 600) {
-        document.getElementById("countdown").innerHTML ="Welcome to NT! James and Matthew are both heading to Waterloo! ϙ2 begins in: 
-        " + minutes + "m " + seconds + "s ";
-  }
-  
-  else if (distance > 60) {
-        document.getElementById("countdown").innerHTML ="Welcome to NT! James should be pulling into Waterloo any minute now! ϙ2 begins in: 
-        " + minutes + "m " + seconds + "s ";
-  }
-  
-  else if (distance > 0) {
-        document.getElementById("countdown").innerHTML = seconds ;
   }
   
   else if (distance > -86400) {
