f26cd7e1904d1aed511fbbf2026850ff52986cf8
malawrence1020 <57117279+malawrence1020@users.noreply.github.com>
2020-03-13 20:04:20 +0000

    Time again again

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Mar 05, 2020 12:00:00").getTime();
+var countDownDate = new Date("Mar 05, 2020 08:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
