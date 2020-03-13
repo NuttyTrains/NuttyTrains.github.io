a0efdd258e1a500687d812a67c08ea2f6aaf8b2f
malawrence1020 <57117279+malawrence1020@users.noreply.github.com>
2020-03-13 19:40:25 +0000

    1000

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -3,13 +3,14 @@ var countDownDate = new Date("Mar 05, 2020 17:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = (countDownDate - now)/1000;
+  var distancetwo = (countDownDate - now);
     
   var days = Math.floor(distance / (86400));
   var hours = Math.floor((distance % (86400)) / (3600));
   var minutes = Math.floor((distance % (3600)) / (60));
   var seconds = Math.floor((distance % (60)));
   
-  var chock = Math.floor(10**(distance/800000 +2));
+  var chock = Math.floor(10**(distancetwo/800000 +2));
     
-  document.getElementById("countdown").innerHTML = " The UK has approx. " + chock + "chock cases. We're all going to die. It was nice knowing you. Reality is an illusion, the universe is a hologram, buy toilet roll!";
+  document.getElementById("countdown").innerHTML = " The UK has approx. " + chock + " chock cases. We're all going to die. Reality is an illusion, the universe is a hologram, buy toilet roll!";
 },1000);
