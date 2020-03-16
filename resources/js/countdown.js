1458b9a117ab0c7a9263923ad2ad51554090c3ca
malawrence1020 <57117279+malawrence1020@users.noreply.github.com>
2020-03-16 19:11:30 +0000

    I think the detty sprinters have been washing their hands

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -9,7 +9,7 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (3600)) / (60));
   var seconds = Math.floor((distance % (60)));
   
-  var chock = Math.floor(10**(distance/750000 +2));
+  var chock = Math.floor(10**(distance/850000 +2));
     
   document.getElementById("countdown").innerHTML = " The UK has " + chock + " chock cases. We're all going to die. Reality is an illusion, the universe is a hologram, buy toilet roll!";
 },1000);
