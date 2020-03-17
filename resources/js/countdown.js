8fd75206f36c2b37abab7f190eca3e8c8facd2b1
James Arthur <jamesarthur7@icloud.com>
2020-03-17 15:45:05 +0000

    Updated Chock Model

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -9,12 +9,12 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (3600)) / (60));
   var seconds = Math.floor((distance % (60)));
 
-  var a = 289.8848724;
+  var a = 315.9848724;
   var b = 152/1000;
 
   var chock = Math.floor(a * Math.exp(b * days));
 
-  var annarchy = Math.floor(((1000/52)*Math.log(0.8*(55980000)/a))/4);
+  var annarchy = Math.floor(((1000/52)*Math.log(0.8*(55980000)/a))/4) - days;
   
 
 document.getElementById("countdown").innerHTML = " The UK has " + chock + " chock cases. We're all going to die and turn to anarchy in " + annarchy + " days. Reality is an illusion, the universe is a hologram, buy toilet roll!"; 
