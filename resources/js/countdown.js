3a30f2905ceb04824185355eeef2c607f42df4cb
malawrence1020 <57117279+malawrence1020@users.noreply.github.com>
2020-12-03 18:21:44 +0000

    Quintillion?

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -8,15 +8,16 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (86400)) / (3600));
   var minutes = Math.floor((distance % (3600)) / (60));
   var seconds = Math.floor((distance % (60)));
+  var days2 = distance / (86400);
 
   var a = 315.9848724;
   var b = 152/1000;
 
-  var chock = Math.floor(a * Math.exp(b * (days+2)));
-  var chock2 = Math.floor(chock/1000000000000000);
+  var chock = Math.floor(a * Math.exp(b * (days2+2)));
+  var chock2 = Math.floor(chock/1000000000000000000);
 
   var anarchy = Math.floor(((1000/52)*Math.log(0.8*(55980000)/a))/4) - days;
   
 
-document.getElementById("countdown").innerHTML = " The UK has " + chock2 + " quadrillion chock cases. We're all going to die and turn to anarchy in " + anarchy + " days. Reality is an illusion, the universe is a hologram, buy toilet roll!"; 
+document.getElementById("countdown").innerHTML = " The UK has " + chock2 + " quintillion chock cases. We're all going to die and turn to anarchy in " + anarchy + " days. Reality is an illusion, the universe is a hologram, buy toilet roll!"; 
 },1000);
