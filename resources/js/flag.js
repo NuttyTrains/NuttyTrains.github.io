3b199dc377eb0aed7d5251cab0aab8193e374ed4
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-03-20 02:54:04 +0000

    VSCode is unhappy with styleSP.css - I'll sort it later

--- resources/js/flag.js
+++ resources/js/flag.js
@@ -1,15 +1,24 @@
 function ChinaFlag(){
-var id = document.getElementById('Flag'); 
-if(id.style.display == 'block')
-{ id.style.display = 'none'; 
-id.style.visibility = 'hidden'; }
-else{ id.style.display = 'block'; 
-id.style.visibility = 'visible';}}
+    var id = document.getElementById('Flag'); 
+    if(id.style.display == 'block')
+    { id.style.display = 'none'; 
+    id.style.visibility = 'hidden'; }
+    else{ id.style.display = 'block'; 
+    id.style.visibility = 'visible';}}
 
 function ChinaMango(){
-var id = document.getElementById('Mango'); 
-if(id.style.display == 'block')
-{ id.style.display = 'none'; 
-id.style.visibility = 'hidden'; }
-else{ id.style.display = 'block'; 
-id.style.visibility = 'visible';}}
+    var id = document.getElementById('Mango'); 
+    if(id.style.display == 'block')
+    { id.style.display = 'none'; 
+    id.style.visibility = 'hidden'; }
+    else{ id.style.display = 'block'; 
+    id.style.visibility = 'visible';}}
+
+function Gadaffi(){
+    var id = document.getElementById('Gadaffi'); 
+    if(id.style.display == 'block')
+    { id.style.display = 'none'; 
+    id.style.visibility = 'hidden'; }
+    else{ id.style.display = 'block'; 
+    id.style.visibility = 'visible';}}
+    
