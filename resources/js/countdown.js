a5164e3ee6eb7d3b6c68e56213509327b57a6777
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-03-28 23:06:28 +0100

    Our next tweet is our 5000th - make it good @alephJamesA !

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,23 +1,83 @@
 var countDownDate = new Date("Mar 05, 2020 08:00:00").getTime();
+var Digamma1 = new Date("Apr 14, 2021 12:51:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = (now - countDownDate)/1000;
+  var DG1 = (Digamma1 - now)/1000;
     
   var days = Math.floor(distance / (86400));
   var hours = Math.floor((distance % (86400)) / (3600));
   var minutes = Math.floor((distance % (3600)) / (60));
   var seconds = Math.floor((distance % (60)));
   var days2 = distance / (86400);
+    
+  var daysDG1 = Math.ceil((DG1 - 43200) / (86400));
+  var hoursDG1 = Math.floor((DG1 % (86400)) / (3600));
+  var minutesDG1 = Math.floor((DG1 % (3600)) / (60));
+  var secondsDG1 = Math.floor((DG1 % (60)));
 
   var a = 315.9848724;
   var b = 152/1000;
 
   var chock = a * Math.exp(b * (days2+2));
   var chock2 = Math.floor(chock/1000000000000000000000);
-
-  var anarchy = Math.floor(((1000/52)*Math.log(0.8*(55980000)/a))/4) - days;
   
 
-document.getElementById("countdown").innerHTML = " The UK has " + chock2 + " sextillion chock cases. We're all going to die and turn to anarchy in " + anarchy + " days. Reality is an illusion, the universe is a hologram, buy toilet roll!"; 
+  if (DG1 > 86400+43200) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The UK has " + chock2 + " sextillion chock cases. Ϝ1 begins in " + daysDG1 + " days!"
+  }
+
+  else if (DG1 > 43200) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The UK has " + chock2 + " sextillion chock cases. Ϝ1 is tomorrow!";
+  }
+
+  else if (DG1 > 16200) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! Ϝ1 begins in: " + hoursDG1 + "h " + minutesDG1 + "m " + secondsDG1 + "s ";
+  }
+
+  else if (DG1 > 12600) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! Matthew is on his way to PAD and James is heading to CBN: Ϝ1 begins in: " + hoursDG1 + "h " + minutesDG1 + "m " + secondsDG1 + "s ";
+  }
+
+  else if (DG1 > 6600) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! We're off! Matthew is travelling to Brizzle and James to Plymouth! Ϝ1 begins in: " + hoursDG1 + "h " + minutesDG1 + "m " + secondsDG1 + "s ";
+  }
+
+  else if (DG1 > 3600) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! Matthew is currently waiting in Brizzle and James is travelling to Exeter! Ϝ1 begins in: " + hoursDG1 + "h " + minutesDG1 + "m " + secondsDG1 + "s ";
+  }
+
+  else if (DG1 > 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! We are both closing in on Taunton! Ϝ1 begins in: " + minutesDG1 + "m " + secondsDG1 + "s!";
+  }
+
+  else if (DG1 > -4800) {
+    document.getElementById("countdown").innerHTML = "IT IS NT TIME!!! Matthew and James are in Taunton!";
+  }
+
+  else if (DG1 > -6600) {
+    document.getElementById("countdown").innerHTML = "IT IS NT TIME!!! Matthew and James are travelling to Weston-super-mare!";
+  }
+
+  else if (DG1 > -8400) {
+    document.getElementById("countdown").innerHTML = "IT IS NT TIME!!! Matthew and James are in Weston-super-mare!";
+  }
+
+  else if (DG1 > -10800) {
+    document.getElementById("countdown").innerHTML = "IT IS NT TIME!!! Matthew and James are travelling to Brizzle!";
+  }
+
+  else if (DG1 > -18000) {
+    document.getElementById("countdown").innerHTML = "IT IS NT TIME!!! Matthew and James are in Brizzle!";
+  }
+
+  else if (DG1 > -36000) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! We are both travelling home from Ϝ1";
+  }
+
+  else {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The UK has " + chock2 + " sextillion chock cases. Reality is an illusion, the universe is a hologram, buy toilet roll!";
+  }
+
 },1);
