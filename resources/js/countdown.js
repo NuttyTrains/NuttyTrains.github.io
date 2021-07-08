50ee7ebc838dc53d3d5aba7d1697022c4991ae9b
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-07-08 16:21:05 +0100

    Fix cock up

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -12,7 +12,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (60)));
   var days2 = distance / (86400);
 
-  var daysF = Math.floor(FD / (86400));
+  var daysF = Math.ceil(FD / (86400));
   
   var a = 315.9848724;
   var b = 152/1000;
@@ -21,19 +21,19 @@ var x = setInterval(function() {
   var chock2 = Math.floor(chock/1000000000000000000000000000);
   
 
-  if (daysF > 0) {
+  if (daysF > 1) {
     document.getElementById("countdown").innerHTML = "Welcome to NT! The UK has " + chock2 + " octillion cases of chock. All restrictions end in " + daysF + " days! Reality is an illusion, the universe is a hologram, buy toilet roll!";
   }
  
-  else if (daysF == 0) {
+  else if (daysF == 1) {
     document.getElementById("countdown").innerHTML = "Welcome to NT! The UK has " + chock2 + " octillion cases of chock. All restrictions end tomorrow! Reality is an illusion, the universe is a hologram, buy toilet roll!";
   }
  
-  else if (daysF > -4) {
+  else if (daysF > -3) {
     document.getElementById("countdown").innerHTML = "Welcome to NT! All chock restrictions have ended! Reality is an illusion, the universe is a hologram, buy toilet roll!";
   }
  
-  else if (daysF > -15) {
+  else if (daysF > -14) {
     document.getElementById("countdown").innerHTML = "Farewell ChockClock, you've been a friend throughout. BBB! Reality is an illusion, the universe is a hologram, buy toilet roll! - ChockClock, 2020-2021";
   }
  
