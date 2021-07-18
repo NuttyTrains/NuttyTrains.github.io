ee5592977ee525f031b6ddc589b57e096bcc6a92
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-07-18 01:00:08 +0100

    TEST 5.0

--- resources/js/table.js
+++ resources/js/table.js
@@ -19,3 +19,30 @@ function getAjax(CSV, Div) {
         }
     });
   }
+
+function arrayToTable45(tableData) {
+    var table = $('<table></table>');
+    $(tableData).each(function (i, rowData) {
+        var row = $('<tr></tr>');
+        $(rowData).each(function (j, cellData) {
+            if (j<4){
+            row.append($('<td>'+cellData+'</td>'));
+            }
+            else{
+            row.append($('<td>'+cellData.split('/').join('<br>')+'</td>'));
+            }
+        });
+        table.append(row);
+    });
+    return table;
+}
+
+function getAjax45(CSV, Div) {
+    $.ajax({
+        type: "GET",
+        url: CSV,
+        success: function (data) {
+            $(Div).append(arrayToTable45(Papa.parse(data).data));
+        }
+    });
+  }
