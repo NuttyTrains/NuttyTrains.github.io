d77eb1bd2444f28816942637e2603f649a09ee77
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-07-18 03:15:51 +0100

    TEST 9.0

--- resources/js/flag.js
+++ resources/js/flag.js
@@ -22,3 +22,10 @@ function Gadaffi(){
     else{ id.style.display = 'block'; 
     id.style.visibility = 'visible';}}
     
+function China(id1){
+    var id = document.getElementById(id1); 
+    if(id.style.display == 'block')
+    { id.style.display = 'none'; 
+    id.style.visibility = 'hidden'; }
+    else{ id.style.display = 'block'; 
+    id.style.visibility = 'visible';}}
