93cd359ef4bf31e00f2bf2cbc67eff52f0c76e25
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-07-18 03:22:56 +0100

    TEST 9.1

--- resources/js/flag.js
+++ resources/js/flag.js
@@ -1,27 +1,3 @@
-function ChinaFlag(){
-    var id = document.getElementById('Flag'); 
-    if(id.style.display == 'block')
-    { id.style.display = 'none'; 
-    id.style.visibility = 'hidden'; }
-    else{ id.style.display = 'block'; 
-    id.style.visibility = 'visible';}}
-
-function ChinaMango(){
-    var id = document.getElementById('Mango'); 
-    if(id.style.display == 'block')
-    { id.style.display = 'none'; 
-    id.style.visibility = 'hidden'; }
-    else{ id.style.display = 'block'; 
-    id.style.visibility = 'visible';}}
-
-function Gadaffi(){
-    var id = document.getElementById('Gadaffi'); 
-    if(id.style.display == 'block')
-    { id.style.display = 'none'; 
-    id.style.visibility = 'hidden'; }
-    else{ id.style.display = 'block'; 
-    id.style.visibility = 'visible';}}
-    
 function China(id1){
     var id = document.getElementById(id1); 
     if(id.style.display == 'block')
