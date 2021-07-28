378c6a97cdaef89565552e496a595ce7cfef277a
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-07-28 17:01:08 +0100

    BB Me No Tweety

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,44 +1,55 @@
-var countDownDate = new Date("Mar 05, 2020 08:00:00").getTime();
-var Freedom = new Date("Jul 19, 2021 00:00:00").getTime();
+var Xi2 = new Date("Jul 29, 2021 12:00:00").getTime();
+var Koppa6 = new Date("Aug 1, 2021 11:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distance = (now - countDownDate)/1000;
-  var FD = (Freedom - now)/1000;
-   
-  var days = Math.floor(distance / (86400));
-  var hours = Math.floor((distance % (86400)) / (3600));
-  var minutes = Math.floor((distance % (3600)) / (60));
-  var seconds = Math.floor((distance % (60)));
-  var days2 = distance / (86400);
+  var distanceX2 = (Xi2 - now)/1000;
+  var distanceK6 = (Koppa6 - now)/1000;
 
-  var daysF = Math.ceil(FD / (86400));
-  
-  var a = 315.9848724;
-  var b = 152/1000;
+  var daysX2 = Math.ceil(distanceX2 / (86400));
+  var hoursX2 = Math.ceil(distanceX2 / (3600));
+  var elapsedX2 = -1*hoursX2;
+  var hoursK6 = Math.ceil(distanceK6 / (3600));
+  var elapsedK6 = -1*hoursK6;
 
-  var chock = a * Math.exp(b * (days2+2));
-  var chock2 = Math.floor(chock/1000000000000000000000000000);
-  
+  if (daysX2 > 1) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM DAI in " + daysX2 + " days is the XI 2 service to OXFORD, calling at ISIS, PUNTING, WOZZA, and THE MIDNIGHT TRAIN.";
+  }
+ 
+  else if (hoursX2 > 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursX2 + " hours is the XI 2 service to OXFORD, calling at ISIS, PUNTING, WOZZA, and THE MIDNIGHT TRAIN.";
+  }
+ 
+  else if (hoursX2 > -4) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedX2 + " hours into XI 2! The train arriving at PLATFORM DAI is the XI 2 service to OXFORD, calling at ISIS, CYCLING, and ISLIP (AGAIN).";
+  }
 
-  if (daysF > 1) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The UK has " + chock2 + " octillion cases of chock. 'All' restrictions end in " + daysF + " days! Reality is an illusion, the universe is a hologram, buy toilet roll!";
+  else if (hoursX2 > -9) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedX2 + " hours into XI 2! The train arriving at PLATFORM DAI is the XI 2 service to HEREFORD, calling at JEZZA'S FARM, SHITTY LITTLE COTSWOLDS STATIONS and WOZZA.";
+  }
+
+  else if (hoursX2 > -13) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedX2 + " hours into XI 2! The train arriving at PLATFORM DAI is the XI 2 service back to OXFORD, calling at WOZZA, JEZZA'S FARM, and THE MIDNIGHT TRAIN.";
   }
  
-  else if (daysF == 1) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The UK has " + chock2 + " octillion cases of chock. 'All' restrictions end tomorrow! Reality is an illusion, the universe is a hologram, buy toilet roll!";
+  else if (hoursX2 > -20) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedX2 + " hours into XI 2! The train arriving at PLATFORM DAI is the XI 2 service to SLEEP, calling at SLEEP, SLEEP and SLEEP.";
   }
  
-  else if (daysF > -3) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! 'All' chock restrictions have ended! Reality is an illusion, the universe is a hologram, buy toilet roll!";
+  else if (hoursX2 > -30) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedX2 + " hours into XI 2! The train arriving at PLATFORM DAI is the XI 2 service to OXFORD, calling at ISIS, PUNTING and DIDCOT (MAYBE).";
   }
- 
-  else if (daysF > -14) {
-    document.getElementById("countdown").innerHTML = "Farewell ChockClock, you've been a friend throughout. BBB! Reality is an illusion, the universe is a hologram, buy toilet roll! - ChockClock, 2020-2021";
+
+  else if (hoursK6 > 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursK6 + " hours is the KOPPA 6 service to SOMEWHERE IN LONDON, calling at BOWIS, DEL BOY, GLORIOUS LEADER KIM, and MAYBE SOME OTHER PLACES, IDK.";
   }
- 
+
+  else if (hoursK6 > -8) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedK6 + " hours into KOPPA 6! The train arriving at PLATFORM DAI is the KOPPA 6 service to SOMEWHERE IN LONDON, calling at BOWIS, DEL BOY, GLORIOUS LEADER KIM, and MAYBE SOME OTHER PLACES, IDK.";
+  }
+
   else {
-    document.getElementById("countdown").innerHTML = "Welcome to NT!";
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM DAI is the XI 3 service to CAMBRIDGE, calling at DEPRESSION, DEPRESSION, DEPRESSION and DEATH.";
   }
 
 },1);
