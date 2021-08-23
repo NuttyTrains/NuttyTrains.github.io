8a1fbce115872acf9d9722e6ee17b3dca7a9bf61
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-08-23 01:51:19 +0100

    Banner: activate!

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,5 +1,5 @@
-var Xi4 = new Date("Aug 14, 2021 09:00:00").getTime();
-var Koppa8 = new Date("Aug 15, 2021 10:00:00").getTime();
+var Xi4 = new Date("Aug 28, 2021 10:00:00").getTime();
+var Koppa8 = new Date("Aug 29, 2021 12:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
@@ -13,23 +13,23 @@ var x = setInterval(function() {
   var elapsedK8 = -1*hoursK8;
 
   if (daysX4 > 1) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM DAI in " + daysX4 + " days is the XI 3 service to CAMBRIDGE, calling at LONDON, SOME PLACES ON THE WAY TO CAMBRIDGE, DEPRESSION, and DEATH.";
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM DAI in " + daysX4 + " days is the XI 4 service to BRUMMY, calling at OXFORD, WOZZA, STUPID TINY TRAIN, and CAT.";
   }
  
   else if (hoursX4 > 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursX4 + " hours is the XI 3 service to CAMBRIDGE, calling at LONDON, SOME PLACES ON THE WAY TO CAMBRIDGE, DEPRESSION, and DEATH.";
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursX4 + " hours is the XI 4 service to BRUMMY, calling at OXFORD, WOZZA, STUPID TINY TRAIN, and CAT.";
   }
  
-  else if (hoursX4 > -10) {
-    document.getElementById("countdown").innerHTML = "We are " + elapsedX4 + " hours into XI 3! The train arriving at PLATFORM DAI is the XI 3 service to CAMBRIDGE, calling at LONDON, SOME PLACES ON THE WAY TO CAMBRIDGE, DEPRESSION, and DEATH.";
+  else if (hoursX4 > -13) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedX4 + " hours into XI 4! The train arriving at PLATFORM DAI is the XI 4 service to BRUMMY, calling at OXFORD, WOZZA, STUPID TINY TRAIN, and CAT.";
   }
 
   else if (hoursK8 > 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursK8 + " hours is the KOPPA 7 service to SAAAAF LONDON, calling at NORTHERN LINE, TRAMS, CROYDON, and IKEA";
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursK8 + " hours is the KOPPA 8 service to ABANDONMENT, calling at MYSTERY TERMINUS 1, MYSTERY TERMINUS 2, BIKES, EAST LONDON, and SOME OTHER STUFF, IDK";
   }
 
-  else if (hoursK8 > -8) {
-    document.getElementById("countdown").innerHTML = "We are " + elapsedK8 + " hours into KOPPA 7! The train arriving at PLATFORM DAI is the KOPPA 7 service to SAAAAF LONDON, calling at NORTHERN LINE, TRAMS, CROYDON, and IKEA.";
+  else if (hoursK8 > -48) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedK8 + " hours into KOPPA 8! The train arriving at PLATFORM DAI is the KOPPA 8 service to ABANDONMENT, calling at MYSTERY TERMINUS 1, MYSTERY TERMINUS 2, BIKES, EAST LONDON, and SOME OTHER STUFF, IDK";
   }
 
   else {
