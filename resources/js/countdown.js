81fd05ca2f5e79f4ba4968608270b81e12ea8fbe
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-09-17 17:10:38 +0100

    Videos

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,23 +1,23 @@
-var Sampi7 = new Date("Sep 03, 2021 11:00:00").getTime();
+var Digamma2 = new Date("Sep 25, 2021 10:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distanceS7 = (Sampi7 - now)/1000;
+  var distanceF2 = (Digamma2 - now)/1000;
 
-  var daysS7 = Math.ceil(distanceS7 / (86400));
-  var hoursS7 = Math.ceil(distanceS7 / (3600));
-  var elapsedS7 = -1*hoursS7;
+  var daysF2 = Math.ceil(distanceF2 / (86400));
+  var hoursF2 = Math.ceil(distanceF2 / (3600));
+  var elapsedF2 = -1*hoursF2;
 
-  if (daysS7 > 1) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM DAI in " + daysS7 + " days is the SAMPI 7 service to GILFACH FARGOED, calling at TREHERBERT, PONTYPRIDD OFF-LICENCE, CAERPHILTHY CRACK-DEN and <a href='https://www.redbubble.com/i/shower-curtain/Pacer-Memorial-by-NuttyTrains/78376475.YH6LW'>THE PACER MEMORIAL<sup>TM</sup></a>";
+  if (daysF2 > 1) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM ONE (RIP Platform 2) in " + daysF2 + " days is the DIGAMMA 2 service to PILNING(!!), calling at BRIZZLE, SEVERN BEACH and GLOZZA";
   }
  
-  else if (hoursS7 > 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursS7 + " hours is the SAMPI 7 service to GILFACH FARGOED, calling at TREHERBERT, PONTYPRIDD OFF-LICENCE, CAERPHILTHY CRACK-DEN and <a href='https://www.redbubble.com/i/shower-curtain/Pacer-Memorial-by-NuttyTrains/78376475.YH6LW'>THE PACER MEMORIAL<sup>TM</sup></a>";
+  else if (hoursF2 > 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM ONE (RIP Platform 2) in " + hoursF2 + " hours is the DIGAMMA 2 service to PILNING(!!), calling at BRIZZLE, SEVERN BEACH and GLOZZA";
   }
  
-  else if (hoursS7 > -10) {
-    document.getElementById("countdown").innerHTML = "We are " + elapsedS7 + " hours into XI 4! The train arriving at PLATFORM DAI is the SAMPI 7 service to GILFACH FARGOED, calling at TREHERBERT, PONTYPRIDD OFF-LICENCE, CAERPHILTHY CRACK-DEN and <a href='https://www.redbubble.com/i/shower-curtain/Pacer-Memorial-by-NuttyTrains/78376475.YH6LW'>THE PACER MEMORIAL<sup>TM</sup></a>";
+  else if (hoursF2 > -11) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedF2 + " hours into DIGAMMA 2! The train arriving at PLATFORM ONE (RIP Platform 2) is the DIGAMMA 2 service to PILNING(!!), calling at BRIZZLE, SEVERN BEACH and GLOZZA";
   }
 
   else {
