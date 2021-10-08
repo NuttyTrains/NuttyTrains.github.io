f76d8d9599381ef080fe5bf46c8c89cdd5a640f6
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2021-10-08 16:43:01 +0100

    Are we ready for Koppa 9? Fuck no!

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,23 +1,23 @@
-var Digamma2 = new Date("Sep 25, 2021 10:00:00").getTime();
+var Koppa9 = new Date("Oct 09, 2021 13:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distanceF2 = (Digamma2 - now)/1000;
+  var distanceK9 = (Koppa9 - now)/1000;
 
-  var daysF2 = Math.ceil(distanceF2 / (86400));
-  var hoursF2 = Math.ceil(distanceF2 / (3600));
-  var elapsedF2 = -1*hoursF2;
+  var daysK9 = Math.ceil(distanceK9 / (86400));
+  var hoursK9 = Math.ceil(distanceK9 / (3600));
+  var elapsedK9 = -1*hoursK9;
 
-  if (daysF2 > 1) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM ONE (RIP Platform 2) in " + daysF2 + " days is the DIGAMMA 2 service to PILNING(!!), calling at BRIZZLE, SEVERN BEACH and GLOZZA";
+  if (daysK9 > 1) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The next train to arrive at PLATFORM DAI in " + daysK9 + " days is the KOPPA 9 service to BATTERSEA POWER STATION STATION, calling at COCKFOSTERS, HAMMERSMITH, BROMPTON and NINE ELMS";
   }
  
-  else if (hoursF2 > 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM ONE (RIP Platform 2) in " + hoursF2 + " hours is the DIGAMMA 2 service to PILNING(!!), calling at BRIZZLE, SEVERN BEACH and GLOZZA";
+  else if (hoursK9 > 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! The train arriving at PLATFORM DAI in " + hoursK9 + " hours is the KOPPA 9 service to BATTERSEA POWER STATION STATION, calling at COCKFOSTERS, HAMMERSMITH, BROMPTON and NINE ELMS";
   }
  
-  else if (hoursF2 > -11) {
-    document.getElementById("countdown").innerHTML = "We are " + elapsedF2 + " hours into DIGAMMA 2! The train arriving at PLATFORM ONE (RIP Platform 2) is the DIGAMMA 2 service to PILNING(!!), calling at BRIZZLE, SEVERN BEACH and GLOZZA";
+  else if (hoursK9 > -8) {
+    document.getElementById("countdown").innerHTML = "We are " + elapsedK9 + " hours into KOPPA 9! The train arriving at PLATFORM DAI is the KOPPA 9 service to BATTERSEA POWER STATION STATION, calling at COCKFOSTERS, HAMMERSMITH, BROMPTON and NINE ELMS";
   }
 
   else {
