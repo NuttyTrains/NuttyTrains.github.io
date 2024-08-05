acc5007d791598227bc7a6c17482dbf54ded7b8c
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2024-08-05 02:25:20 +0100

    Like I said, never trust drunk coding

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -8,26 +8,28 @@ var x = setInterval(function() {
 
   var daysOx24 = Math.floor(distanceOx24 / 86400);
   var hoursOx24 = Math.floor((distanceOx24 % 86400) / 3600);
+  var elapsedOx24 = hoursOx24*(-1);
+  var elapsedOx24b = elapsedOx24-24;
 
   var daysEdin = Math.floor(distanceEdin / 86400);
 
-  if (daysOx24 < 5) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The next train will be arriving in " + daysOx24 + " days! We will be calling at Oxford et al. Platform DAI for the AUGUST 24 service to ???.";
+  if (daysOx24 < 3) {
+    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The next train will be arriving in " + daysOx24 + " days! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
   }
 
   else if (daysOx24 == 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The next train will be arriving in " + hoursOx24 + " hours! We will be calling at Oxford et al. Platform DAI for the AUGUST 24 service to ???.";
+    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The next train will be arriving in " + hoursOx24 + " hours! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
   }
 
-  else if (hoursOx24 > -13) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! We are " + elapsedOx24 + " hours into August 24! We will be calling at ???. Platform DAI for the AUGUST 24 service to ???.";
+  else if (hoursOx24 > -12 && hoursOx24 < 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! We are " + elapsedOx24 + " hours into Koppa 18! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
   }
 
-  else if (hoursOx24 > -24) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The destination of this AUGUST 24 service has changed. This is now an AUGUST 24 service to ???. We are sorry for any inconvenience this may cause to your journey.";
+  else if (hoursOx24 > -24 && hoursOx24 < 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The destination of this KOPPA 18 service has changed. This is now an AUGUST 24 service to ???. We are sorry for any inconvenience this may cause to your journey.";
   }
 
-  else if (hoursOx24 > -32) {
+  else if (hoursOx24 > -34 && hoursOx24 < 0) {
     document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! We are " + elapsedOx24b + " hours into August 24! We will be calling at Oxford et al. Platform DAI for the AUGUST 24 service to ???.";
   }
 
