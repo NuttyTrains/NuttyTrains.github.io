7cbf2f84b82c7c12b8ef2d29eed13a1cb09532c8
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2024-12-04 19:17:06 +0000

    https://nebula.tv/videos/jetlag-we-played-hide-and-seek-across-japan

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,48 +1,50 @@
-var Ox24 = new Date("Aug 10, 2024 10:00:00").getTime();
-var Edin = new Date("Aug 19, 2024 10:00:00").getTime();
+var Xmas24 = new Date("Dec 06, 2024 09:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distanceOx24 = (Ox24 - now)/1000;
-  var distanceEdin = (Edin - now)/1000;
+  var distanceXmas24 = (Xmas24 - now)/1000;
 
-  var daysOx24 = Math.floor(distanceOx24 / 86400);
-  var hoursOx24 = Math.floor(distanceOx24 / 3600);
-  var elapsedOx24 = hoursOx24*(-1);
-  var elapsedOx24b = elapsedOx24-24;
+  var daysXmas24 = Math.floor(distanceXmas24 / 86400);
+  var hoursXmas24 = Math.floor(distanceXmas24 / 3600);
+  var elapsedXmas24 = hoursXmas24*(-1);
 
   var daysEdin = Math.floor(distanceEdin / 86400);
 
-  if (daysOx24 < 3 && daysOx24 > 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The next train will be arriving in " + daysOx24 + " days! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
+  if (hoursXmas24 > 16 && hoursXmas24 < 72) {
+    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! James is enroute to Milan! We begin NT Xmas 24 in " + elapsedXmas24 + " hours! We will be calling at vin, glühwein and vino.";
   }
 
-  else if (daysOx24 == 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The next train will be arriving in " + hoursOx24 + " hours! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
+  else if (hoursXmas24 > 0 && hoursXmas24 < 24) {
+    //document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! We are " + elapsedOx24 + " hours into Koppa 18! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
+    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! James and Matthew are enroute to Milan! We begin NT Xmas 24 in " + elapsedXmas24 + " hours! We will be calling at vin, glühwein and vino.";
   }
 
-  else if (hoursOx24 > -12 && hoursOx24 < 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! We are " + elapsedOx24 + " hours into Koppa 18! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
+  else if (hoursXmas24 > -3 && hoursXmas24 < 0) {
+    document.getElementById("countdown").innerHTML = "Benvenuti in Italia! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
   }
 
-  else if (hoursOx24 > -24 && hoursOx24 < 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! The destination of this KOPPA 18 service has changed. This is now an AUGUST 24 service to ???. We are sorry for any inconvenience this may cause to your journey.";
+  else if (hoursXmas24 > -6 && hoursXmas24 < 0) {
+    document.getElementById("countdown").innerHTML = "Benvenuti in Svizzera! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
   }
 
-  else if (hoursOx24 > -34 && hoursOx24 < 0) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! We are " + elapsedOx24b + " hours into August 24! We will be calling at Oxford et al. Platform DAI for the AUGUST 24 service to ???.";
+  else if (hoursXmas24 > -52 && hoursXmas24 < 0) {
+    document.getElementById("countdown").innerHTML = "Wilkommen in der Schweiz! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
   }
 
-  else if (daysEdin > 0) {
-    document.getElementById("countdown").innerHTML = "This show contains no maths (and other lies): James will be speaking at Edinburgh Fringe in " + daysEdin + " days! Buy your tickets <a href='https://fringe.alephjamesa.co.uk/'>here!</a>";
+  else if (hoursXmas24 > -78 && hoursXmas24 < 0) {
+    document.getElementById("countdown").innerHTML = "Willkommen in Deutschland! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
   }
 
-  else if (daysEdin > -5) {
-    document.getElementById("countdown").innerHTML = "This show contains no maths (and other lies): James is speaking at Edinburgh Fringe RIGHT NOW! Buy your tickets <a href='https://fringe.alephjamesa.co.uk/'>here!</a>";
+  else if (hoursXmas24 > -96 && hoursXmas24 < 0) {
+    document.getElementById("countdown").innerHTML = "Bienvenue en France! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
+  }
+
+  else if (hoursXmas24 > -100 && hoursXmas24 < 0) {
+    document.getElementById("countdown").innerHTML = "Welcome back to London! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
   }
 
   else {
-    document.getElementById("countdown").innerHTML = "Welcome to NT!";
+    document.getElementById("countdown").innerHTML = "Welcome to NT! Happy Christmas!";
   }
 
 },1000);
