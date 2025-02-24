ba3e6ecba74f013f978850ded2042bff701988b8
Matthew Lawrence <Matthew.X.Lawrence@protonmail.ch>
2025-02-24 02:17:31 +0000

    Coming soon: NT White Rose 2?

--- resources/js/countdown.js
+++ resources/js/countdown.js
@@ -1,44 +1,19 @@
-var Xmas24 = new Date("Dec 06, 2024 09:00:00").getTime();
+var WR2 = new Date("Feb 25 2025 09:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distanceXmas24 = (Xmas24 - now)/1000;
+  var distanceWR2 = (WR2 - now)/1000;
 
-  var daysXmas24 = Math.floor(distanceXmas24 / 86400);
-  var hoursXmas24 = Math.floor(distanceXmas24 / 3600);
-  var elapsedXmas24 = hoursXmas24*(-1);
+  var daysWR2 = Math.floor(distanceWR2 / 86400);
+  var hoursWR2 = Math.floor(distanceWR2 / 3600);
+  var elapsedWR2 = hoursWR2*(-1);
 
-  if (hoursXmas24 > 16 && hoursXmas24 < 72) {
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! James is enroute to Milan! We begin NT Xmas 24 in " + hoursXmas24 + " hours! We will be calling at vin, glühwein and vino.";
+  if (hoursWR2 > 0 && hoursWR2 < 72) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! We begin White Rose 2 in " + hoursWR2 + " hours! We will be calling at Leeds and some other places, idk how long this service is! Platform DAI for the NT WHITE ROSE 2 service to LEEDS.";
   }
 
-  else if (hoursXmas24 > 0 && hoursXmas24 < 24) {
-    //document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! We are " + elapsedOx24 + " hours into Koppa 18! We will be calling at ???. Platform DAI for the KOPPA 18 service to LONDON.";
-    document.getElementById("countdown").innerHTML = "Welcome to Nutty Trains! James and Matthew are enroute to Milan! We begin NT Xmas 24 in " + hoursXmas24 + " hours! We will be calling at vin, glühwein and vino.";
-  }
-
-  else if (hoursXmas24 > -3 && hoursXmas24 < 0) {
-    document.getElementById("countdown").innerHTML = "Benvenuti in Italia! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
-  }
-
-  else if (hoursXmas24 > -6 && hoursXmas24 < 0) {
-    document.getElementById("countdown").innerHTML = "Benvenuti in Svizzera! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
-  }
-
-  else if (hoursXmas24 > -52 && hoursXmas24 < 0) {
-    document.getElementById("countdown").innerHTML = "Wilkommen in der Schweiz! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
-  }
-
-  else if (hoursXmas24 > -78 && hoursXmas24 < 0) {
-    document.getElementById("countdown").innerHTML = "Willkommen in Deutschland! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
-  }
-
-  else if (hoursXmas24 > -96 && hoursXmas24 < 0) {
-    document.getElementById("countdown").innerHTML = "Bienvenue en France! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
-  }
-
-  else if (hoursXmas24 > -100 && hoursXmas24 < 0) {
-    document.getElementById("countdown").innerHTML = "Welcome back to London! NT Xmas 24 is go! We have been travelling for " + elapsedXmas24 + " hours!";
+  else if (hoursWR2 > -6 && hoursWR2 < 0) {
+    document.getElementById("countdown").innerHTML = "Welcome to NT! We are " + elapsedWR2 + " hours into White Rose 2! We will be calling at Leeds and who knows where else? Not us! Platform DAI for the WHITE ROSE 2 service to LEEDS.";
   }
 
   else {
